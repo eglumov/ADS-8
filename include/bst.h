b47cb8a5e39829bd1813271e04c7b5b609201a6c
eglumov <eugen.glumov@yandex.ru>
2026-05-31 22:22:23 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,84 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template <typename T>
+class BST {
+ public:
+  struct Node {
+    T value;
+    int count;
+    Node* left;
+    Node* right;
+
+    Node(T val) : value(val), count(1), left(nullptr), right(nullptr) {}
+  };
+
+ private:
+  Node* root;
+
+  Node* insert(Node* node, const T& value) {
+    if (node == nullptr) {
+      return new Node(value);
+    }
+    if (value < node->value) {
+      node->left = insert(node->left, value);
+    } else if (value > node->value) {
+      node->right = insert(node->right, value);
+    } else {
+      node->count++;
+    }
+    return node;
+  }
+
+  int getDepth(Node* node) const {
+    if (node == nullptr) {
+      return 0;
+    }
+    int leftDepth = getDepth(node->left);
+    int rightDepth = getDepth(node->right);
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+  }
+
+  Node* searchNode(Node* node, T value) const {
+    if (node == nullptr || node->value == value) {
+      return node;
+    }
+    if (value < node->value) {
+      return searchNode(node->left, value);
+    }
+    return searchNode(node->right, value);
+  }
+
+  void clear(Node* node) {
+    if (node != nullptr) {
+      clear(node->left);
+      clear(node->right);
+      delete node;
+    }
+  }
+
+ public:
+  BST() : root(nullptr) {}
+
+  ~BST() {
+    clear(root);
+  }
+
+  void add(const T& value) {
+    root = insert(root, value);
+  }
+
+  int depth() const {
+    return getDepth(root);
+  }
+
+  bool search(T value) const {
+    return searchNode(root, value) != nullptr;
+  }
+
+  Node* getRoot() const {
+    return root;
+  }
+};
+
 #endif  // INCLUDE_BST_H_
